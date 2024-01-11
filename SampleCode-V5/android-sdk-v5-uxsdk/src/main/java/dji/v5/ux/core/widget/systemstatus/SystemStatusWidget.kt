5584bd7aa50167ffb3bba2700326d7c2a718c97d
Oleksandr <sboyko@iteratorshq.com>
2024-01-11 16:50:44 +0100

    missing import fix

--- SampleCode-V5/android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/core/widget/systemstatus/SystemStatusWidget.kt
+++ SampleCode-V5/android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/core/widget/systemstatus/SystemStatusWidget.kt
@@ -28,6 +28,7 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.Pair
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
